57a1e82d90d76c39a5912c01c02921c836812d12
Michael Williams <michael.williams@enspiral.com>
2017-02-27 18:00:38 +1300

    initial tests

--- test/index.js
+++ test/index.js
@@ -1,8 +1,133 @@
 const test = require('tape')
 
-const depnest = require('../')
+const nest = require('../')
 
-test('depnest', function (t) {
-  t.ok(depnest, 'module is require-able')
+test('depnest', t => {
+  t.ok(nest, 'module is require-able')
+  t.end()
+})
+
+test('nest(string)', t => {
+  const args = ['cats.actions.save']
+  const expected = { cats: { actions: { save: true } } }
+  const actual = nest(...args)
+  t.deepEqual(actual, expected, 'actual equals expected')
+  t.end()
+})
+
+test('nest(string, string)', t => {
+  const args = ['cats.actions.set', 'first']
+  const expected = { cats: { actions: { set: 'first' } } }
+  const actual = nest(...args)
+  t.deepEqual(actual, expected, 'actual equals expected')
+  t.end()
+})
+
+test('nest(string, function)', t => {
+  const fn = () => {}
+  const args = ['cats.actions.set', fn]
+  const expected = { cats: { actions: { set: fn } } }
+  const actual = nest(...args)
+  t.deepEqual(actual, expected, 'actual equals expected')
+  t.end()
+})
+
+test('nest(string, array)', t => {
+  const args = ['cats.actions', [
+    'create',
+    'save'
+  ]]
+  const expected = {
+    cats: {
+      actions: {
+        create: true,
+        save: true
+      }
+    }
+  }
+  const actual = nest(...args)
+  t.deepEqual(actual, expected, 'actual equals expected')
+  t.end()
+})
+
+test('nest(string, object)', t => {
+  const create = () => {}
+  const save = () => {}
+  const args = ['cats.actions', {
+    create,
+    save
+  }]
+  const expected = {
+    cats: {
+      actions: {
+        create,
+        save
+      }
+    }
+  }
+  const actual = nest(...args)
+  t.deepEqual(actual, expected, 'actual equals expected')
+  t.end()
+})
+
+test('nest(object) with array', t => {
+  const args = [{
+    'cats.actions': [
+      'create',
+      'save'
+    ]
+  }]
+  const expected = {
+    cats: {
+      actions: {
+        create: true,
+        save: true
+      }
+    }
+  }
+  const actual = nest(...args)
+  t.deepEqual(actual, expected, 'actual equals expected')
+  t.end()
+})
+
+test('nest(object) with strings', t => {
+  const args = [{
+    'cats.actions': {
+      create: 'first',
+      save: 'first'
+    }
+  }]
+  const expected = {
+    cats: {
+      actions: {
+        create: 'first',
+        save: 'first'
+      }
+    }
+  }
+  const actual = nest(...args)
+  t.deepEqual(actual, expected, 'actual equals expected')
+  t.end()
+})
+
+test('nest(object) with functions', t => {
+  const create = () => {}
+  const save = () => {}
+  const args = [{
+    'cats.actions': {
+      create,
+      save
+    }
+  }]
+  const expected = {
+    cats: {
+      actions: {
+        create,
+        save
+      }
+    }
+  }
+  const actual = nest(...args)
+  t.deepEqual(actual, expected, 'actual equals expected')
   t.end()
 })
